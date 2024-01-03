75f5d76efdccc478c81a7b2e712b2cbbbe41004e
Isaías Santana <isds.santana@gmail.com>
2024-01-03 14:37:30 -0300

    Initial Commit

---
+++ StepCounter/StepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterApp.swift
+//  StepCounter
+//
+//  Created by Isaías Santana on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
